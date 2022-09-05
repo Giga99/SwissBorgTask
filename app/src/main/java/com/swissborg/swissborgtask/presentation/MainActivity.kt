0b5fb02a369a32efbff60df4489eba60a64cdd2b
Giga99 <igorstevanovic99@gmail.com>
2022-09-06 00:31:44 +0200

    Initial commit

---
+++ app/src/main/java/com/swissborg/swissborgtask/presentation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.swissborg.swissborgtask.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.swissborg.swissborgtask.presentation.ui.theme.SwissBorgTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SwissBorgTaskTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
